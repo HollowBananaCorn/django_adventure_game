d35119100a08a2abac87c3cdc63d3b9f5d7758ad
Glanyzzz2 <2857959Y@student.gla.ac.uk>
2025-03-26 14:38:09 +0000

    Update forms.py

--- rango/forms.py
+++ rango/forms.py
@@ -5,6 +5,7 @@ from rango.models import *
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
+    confirm_password = forms.CharField(widget=forms.PasswordInput())
 
     class Meta:
         model = User
@@ -14,3 +15,13 @@ class UserProfileForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ('website', 'picture')
+
+ def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
+
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match!")
+
+        return cleaned_data
