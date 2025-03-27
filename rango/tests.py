548411db853a975dc383dc23c2846b9323f4a0d4
Begli Saparaliev <2916431s@student.gla.ac.uk>
2025-03-27 22:32:41 +0000

    added one sample test

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
+from rango.models import Character
+
+class CharacterMethodTests (TestCase):
+
+    def test_health_equals_provided(self):
+
+        """
+        Test to make sure the character's health cannot exceed 100
+        """
+        character = Character(current_health=100)
+        character.save()
+
+        self.assertEqual((character.current_health ==100), True)
+
 # Create your tests here.
