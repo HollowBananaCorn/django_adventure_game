f19ee8fc67c287cd97ff0cb8d7300f40d550baf7
Begli Saparaliev <2916431s@student.gla.ac.uk>
2025-03-28 16:19:28 +0000

    all tests done

--- rango/tests.py
+++ rango/tests.py
@@ -7,7 +7,6 @@ from rango.models import Action
 from django.contrib.auth.models import User
 from django.urls import reverse
 
-# Create your tests here.
 class CharacterMethodTests (TestCase):
 
     def test_for_defaults(self):
@@ -45,12 +44,20 @@ class EnemyTests(TestCase):
 
     def test_str_method(self):
 
+        """
+        Enemy str method test
+        """
+
         enemy = Enemy()
         enemy.save()
 
         self.assertEqual(enemy.__str__(), enemy.name)
 
     def test_imageUrl_method(self):
+
+        """
+        Enemy imageUrl method test
+        """
 
         enemy = Enemy()
         enemy.save()
@@ -60,6 +67,10 @@ class EnemyTests(TestCase):
 class ActionTests(TestCase):
 
     def test_str_method(self):
+        
+        """
+        Action str method test
+        """
 
         action = Action()
         action.save()
@@ -70,34 +81,116 @@ class ActionTests(TestCase):
 class IndexViewTests(TestCase):
 
     def test_index_view_status_code(self):
+        
+        """
+        Test to check that the index page loads
+        """
 
         response = self.client.get(reverse('rango:index'))
         self.assertEqual(response.status_code, 200)
 
     def test_index_view_response_contains(self):
+        
+        """
+        Test to ensure correct content included in response
+        """
 
         response = self.client.get(reverse('rango:index'))
         self.assertContains(response, "Adventure Game Home")
 
     def test_uses_correct_template(self):
+        
+        """
+        Test to ensure that the correct html template was loaded
+        """
 
         response = self.client.get(reverse('rango:index'))
         self.assertTemplateUsed(response, 'rango/index.html')
 
     def test_index_view_context_not_logged_in(self):
+        
+        """
+        Testing index page behaviour of when user is not logged in
+        """
 
         response = self.client.get(reverse('rango:index'))
         self.assertEqual(response.context['boldmessage'], "not logged in")
 
     def test_index_view_context_logged_in(self):
+        
+        """
+        Testing index page behaviour of logged in user
+        """
 
         test_user = User.objects.create_user(username="Jack")
         userProfile = UserProfile(user=test_user)
         userProfile.save()
-
         self.client.force_login(test_user)
 
         response = self.client.get(reverse('rango:index'))
         self.assertEqual(response.context['boldmessage'], "Welcome to your personal game dashboard!")
+
+class LoginViewTests(TestCase):
+    
+    def test_login_view_status_code(self):
         
-# Create your tests here.
+        """
+        Test to check that the login page loads
+        """
+
+        response = self.client.get(reverse('rango:login'))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_login_renders_properly(self): 
+
+        """
+        Test to check content is loaded correctly in login page
+        """       
+
+        response = self.client.get(reverse('rango:login'))
+        self.assertContains(response, "background")
+    
+    def test_uses_correct_template(self):
+        
+        """
+        Test to check that correct html form is used
+        """
+
+        response = self.client.get(reverse('rango:login'))
+        self.assertTemplateUsed(response, "rango/login.html")
+
+
+class UserFormTests(TestCase):
+
+    def test_form_created_with_valid_data(self):
+        
+        """
+        Test to ensure that when user provides valid data, a user object is created
+        """
+
+        form_data = {'username':'Jackson3',
+                     'email':'jack@hotmail.com',
+                     'password': 'mdfa234xjei2'}
+        
+        response = self.client.post(reverse('rango:register'), data=form_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(User.objects.filter(username="Jackson3").exists())
+    
+    def test_form_not_created(self):
+
+        """
+        Test to see if invalid information in a form is handled correctly. Form should 
+        not be created and form returned in response to correct errors
+        """
+
+        test_user = User.objects.create_user(username='Jack')
+        userProfile = UserProfile(user=test_user)
+        userProfile.save()
+
+        form_data = {'username': 'Jack',
+                     'email':'jack@hotmail.com',
+                     'password': 'rlkfae2p1'}
+        
+        response = self.client.post(reverse('rango:register'), data=form_data)
+        self.assertTrue("user_form" in response.context)
